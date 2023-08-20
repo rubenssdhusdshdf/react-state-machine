1e550d54bd804a9788a79cbe808ee8cd3783fb43
Ruben <rubenvegajusto@gmail.com>
2023-08-20 16:32:59 +0100

    services

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,18 +1,27 @@
-import React,{useState}from 'react';
+import React, { useState } from 'react';
 import './Search.css';
 
-export const Search=({send})=>{const[flight,setFlight]=useState('');const goToPassengers=()=>{send('CONTINUE')}
+export const Search = ({ state, send }) => {
+  const [flight, setFlight] = useState('');
 
-const handleSelectChange=(event)=>{
-  setFlight(event.target.value);
-};
+  const goToPassengers = () => {
+    send('CONTINUE', { selectedCountry: flight })
+  }
 
-const options=['Mexico','Venezuela','Colombia'];
-return(
-  <div className='Search'>
-    <p className='Search-title title'>Busca tu destino</p>
-    <select id="country"className='Search-select'value={flight}onChange={handleSelectChange}>
-      <option value=""disabled defaultValue>Escoge un país</option>{options.map((option)=><option value={option}key={option}>{option}</option>)}
-    </select>
-    <button onClick={goToPassengers}disabled={flight===''}className='Search-continue button'>Continuar</button>
-    </div>);};
+  const handleSelectChange = (event) => {
+    setFlight(event.target.value);
+  };
+
+  const options = state.context.countries;
+
+  return (
+    <div className='Search'>
+      <p className='Search-title title'>Busca tu destino</p>
+      <select id="country" className='Search-select' value={flight} onChange={handleSelectChange}>
+        <option value="" disabled defaultValue>Escoge un país</option>
+        {options.map((option) => <option value={option.name.common} key={option.name.common}>{option.name.common}</option>)}
+      </select>
+      <button onClick={goToPassengers} disabled={flight === ''} className='Search-continue button'>Continuar</button>
+    </div>
+  );
+}; 
---
+++ src/Machines/ejemploJerarquia.js
@@ -0,0 +1,81 @@
+import { createMachine, assign } from "xstate";
+
+const fillCountries = {
+  initial: "loading",
+  states: {
+    loading: {
+      on: {
+        DONE: "success",
+        ERROR: "failure",
+      },
+    },
+    success: {},
+    failure: {
+      on: {
+        RETRY: { target: "loading" },
+      },
+    },
+  },
+};
+
+const bookingMachine = createMachine(
+  {
+    id: "buy plane tickets",
+    initial: "initial",
+    context: {
+      passengers: [],
+      selectedCountry: "",
+    },
+    states: {
+      initial: {
+        on: {
+          START: {
+            target: "search",
+          },
+        },
+      },
+      search: {
+        on: {
+          CONTINUE: {
+            target: "passengers",
+            actions: assign({
+              selectedCountry: (context, event) => event.selectedCountry,
+            }),
+          },
+          CANCEL: "initial",
+        },
+        ...fillCountries,
+      },
+      tickets: {
+        on: {
+          FINISH: "initial",
+        },
+      },
+      passengers: {
+        on: {
+          DONE: "tickets",
+          CANCEL: {
+            target: "initial",
+            actions: "cleanContext",
+          },
+          ADD: {
+            target: "passengers",
+            actions: assign((context, event) =>
+              context.passengers.push(event.newPassenger)
+            ),
+          },
+        },
+      },
+    },
+  },
+  {
+    actions: {
+      cleanContext: assign({
+        selectedCountry: "",
+        passengers: [],
+      }),
+    },
+  }
+);
+
+export default bookingMachine;
