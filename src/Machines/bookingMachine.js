aeab8a664befc2fe0dce02f84d9bee88e2cead4c
Ruben <rubenvegajusto@gmail.com>
2023-08-20 16:10:28 +0100

    actions

--- src/Machines/bookingMachine.js
+++ src/Machines/bookingMachine.js
@@ -1,17 +1,28 @@
-import { createMachine } from "xstate";
+import { createMachine, assign } from "xstate";
 
 const bookingMachine = createMachine({
   id: "buy plane tickets",
   initial: "initial",
+  context: {
+    passengers: [],
+    selectedCountry: '',
+  },
   states: {
     initial: {
       on: {
-        START: "search",
+        START: {
+          target: "search",
+        },
       },
     },
     search: {
       on: {
-        CONTINUE: "passengers",
+        CONTINUE: {
+          target: 'passengers',
+          actions: assign({
+            selectedCountry: (context, event) => event.selectedCountry
+          })
+        },
         CANCEL: "initial",
       },
     },
@@ -24,9 +35,23 @@ const bookingMachine = createMachine({
       on: {
         DONE: "tickets",
         CANCEL: "initial",
+        ADD: {
+          target: 'passengers',
+          actions: assign(
+            (context, event) => context.passengers.push(event.newPassenger)
+          )
+        }
       },
     },
   },
-});
+},
+{
+  actions: {
+    imprimirInicio: () => console.log('Imprimir inicio'),
+    imprimirEntrada: () => console.log('Imprimir entrada a search'),
+    imprimirSalida: () => console.log('Imprimir salida del search'),
+  },
+}
+);
 
 export default bookingMachine;
