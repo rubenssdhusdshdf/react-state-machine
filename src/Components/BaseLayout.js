06bcd42f8dc76e29db05be47a6aa4b403fb45eaa
Ruben <rubenvegajusto@gmail.com>
2023-08-20 12:33:33 +0100

    state-machine-react

--- src/Components/BaseLayout.js
+++ src/Components/BaseLayout.js
@@ -2,11 +2,17 @@ import React from 'react';
 import { useMachine } from '@xstate/react';
 import bookingMachine from '../Machines/bookingMachine';
 
-export const BaseLayout = () => {
-  const [state, send] = useMachine(bookingMachine);
+export const BaseLayout = ()=> {
+    const [state,send] = useMachine(bookingMachine)
 
-  console.log('nuestra maquina', state);
-  return (
-    <div>Hola</div>
-  );
+    console.log('state machine', state)
+    console.log('matches(initial): ', state.matches('initial')) // true 
+    console.log('matches(tickets): ', state.matches('tickets')) // false 
+    console.log('can(START): ', state.can('START')) // true 
+    console.log('can(FINISH): ', state.can('FINISH')) // false 
+
+    return (
+        <div>State Machine</div>
+    )
 }
+
